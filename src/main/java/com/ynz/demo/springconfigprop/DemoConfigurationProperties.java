c38cd693f363054c44cf15a2761d0cf947e7b460
yichunzhao <zhaoyichun@hotmail.com>
2020-07-04 09:25:14 +0200

    setup lombok @slfj with logback

--- src/main/java/com/ynz/demo/springconfigprop/DemoConfigurationProperties.java
+++ src/main/java/com/ynz/demo/springconfigprop/DemoConfigurationProperties.java
@@ -1,5 +1,6 @@
 package com.ynz.demo.springconfigprop;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -8,15 +9,19 @@ import org.springframework.util.Assert;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@Slf4j
 public class DemoConfigurationProperties {
 
+
     public static void main(String[] args) {
+        log.info("++++++++++++++++ START  ++++++++++++++++++++");
         ConfigurableApplicationContext context = SpringApplication.run(DemoConfigurationProperties.class, args);
         MailProperties mailProperties = context.getBean(MailProperties.class);
 
         Assert.isTrue(mailProperties.getFrom().equals("mailer@mail.com"), "unexpected value.");
-        Assert.isTrue(mailProperties.getPort().equals("9000"), "unexpected value.");
+        Assert.isTrue(mailProperties.getPort() == 9000, "unexpected value.");
         Assert.isTrue(mailProperties.getHostname().equals("host@mail.com"), "unexpected value.");
-
+        log.info("++++++++++++++++  END  ++++++++++++++++++++");
     }
+
 }
--- src/main/java/com/ynz/demo/springconfigprop/MailProperties.java
+++ src/main/java/com/ynz/demo/springconfigprop/MailProperties.java
@@ -2,16 +2,27 @@ package com.ynz.demo.springconfigprop;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 
 //@Component
 @ConfigurationProperties(prefix = "mail")
 @Setter
 @Getter
+
 public class MailProperties {
+    @NotBlank
     private String hostname;
-    private String port;
+
+    @Min(1000)
+    @Max(9000)
+    private int port;
+
+    @NotBlank
     private String from;
 
 }
